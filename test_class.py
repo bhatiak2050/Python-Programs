a542413025aebc323e506f52c3055bfd69ccc1a7
Karan Bhatia <karanbhatia2050@gmail.com>
2018-03-01 17:02:57 +0530

    New code for Class Menu

--- test_class.py
+++ test_class.py
@@ -1,11 +1,11 @@
-class menu():
-    def __init__(self):
-        self = []
-        n = int(input("How many elements to begin with??"))
+class menu1:
+    def __init__(self, n):
+        self.list1=[]
         for i in range(n):
-            self.append(input("Enter name: "));
+            self.list1.append(str(input("Enter item: ")))
     def getmenu(self):
-        return self
+        return self.list1
 
-ob = menu()
+n = int(input("How many elements to begin with??"))
+ob = menu1(n)
 print(ob.getmenu())
